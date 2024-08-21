3bcbeb79d26dd07cde511f71d69ba03e40bc78a4
Joshua Scarsbrook <jscarsbrook@gmail.com>
2024-08-21 13:41:01 +1000

    removed old login config system

--- cmd/tinyrange/login.go
+++ cmd/tinyrange/login.go
@@ -27,8 +27,6 @@ var (
 	loginMemorySize   int
 	loginStorageSize  int
 	loginExec         string
-	loginLoadConfig   string
-	loginSaveConfig   string
 	loginDebug        bool
 	loginNoScripts    bool
 	loginFiles        []string
@@ -126,242 +124,207 @@ var loginCmd = &cobra.Command{
 			return nil
 		}
 
-		if loginLoadConfig != "" {
-			f, err := os.Open(loginLoadConfig)
+		var pkgs []common.PackageQuery
+
+		for _, arg := range args {
+			q, err := common.ParsePackageQuery(arg)
 			if err != nil {
 				return err
 			}
-			defer f.Close()
 
-			def, err := builder.UnmarshalDefinition(f)
+			pkgs = append(pkgs, q)
+		}
+
+		var dir []common.Directive
+
+		if loginBuilder == "" {
+			return fmt.Errorf("please specify a builder")
+		}
+
+		var tags common.TagList
+
+		tags = append(tags, "level3", "defaults")
+
+		if loginNoScripts || loginWriteRoot != "" {
+			tags = append(tags, "noScripts")
+		}
+
+		planDirective, err := builder.NewPlanDefinition(loginBuilder, pkgs, tags)
+		if err != nil {
+			return err
+		}
+
+		dir = append(dir, planDirective)
+
+		for _, filename := range loginFiles {
+			if strings.HasPrefix(filename, "http://") || strings.HasPrefix(filename, "https://") {
+				parsed, err := url.Parse(filename)
+				if err != nil {
+					return err
+				}
+
+				base := path.Base(parsed.Path)
+
+				dir = append(dir, common.DirectiveAddFile{
+					Definition: builder.NewFetchHttpBuildDefinition(filename, 0),
+					Filename:   path.Join("/root", base),
+				})
+			} else {
+				absPath, err := filepath.Abs(filename)
+				if err != nil {
+					return err
+				}
+
+				dir = append(dir, common.DirectiveLocalFile{
+					HostFilename: absPath,
+					Filename:     path.Join("/root", filepath.Base(absPath)),
+				})
+			}
+		}
+
+		for _, filename := range loginArchives {
+			var def common.BuildDefinition
+			if strings.HasPrefix(filename, "http://") || strings.HasPrefix(filename, "https://") {
+				def = builder.NewFetchHttpBuildDefinition(filename, 0)
+
+				parsed, err := url.Parse(filename)
+				if err != nil {
+					return err
+				}
+
+				filename = parsed.Path
+			} else {
+				hash, err := sha256HashFromFile(filename)
+				if err != nil {
+					return err
+				}
+
+				def = builder.NewConstantHashDefinition(hash, func() (io.ReadCloser, error) {
+					return os.Open(filename)
+				})
+			}
+
+			ark, err := detectArchiveExtractor(def, filename)
+			if err != nil {
+				return err
+			}
+
+			dir = append(dir, common.DirectiveArchive{Definition: ark, Target: "/root"})
+		}
+
+		if loginWriteRoot != "" {
+			dir = append(dir, common.DirectiveBuiltin{Name: "init", GuestFilename: "init"})
+
+			def := builder.NewBuildFsDefinition(dir, "tar")
+
+			ctx := db.NewBuildContext(def)
+
+			f, err := db.Build(ctx, def, common.BuildOptions{})
 			if err != nil {
 				slog.Error("fatal", "err", err)
 				os.Exit(1)
 			}
 
-			if _, err := db.Build(db.NewBuildContext(def), def, common.BuildOptions{
+			fh, err := f.Open()
+			if err != nil {
+				return err
+			}
+			defer fh.Close()
+
+			out, err := os.Create(path.Base(loginWriteRoot))
+			if err != nil {
+				return err
+			}
+			defer out.Close()
+
+			if _, err := io.Copy(out, fh); err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		if loginExec != "" {
+			dir = append(dir, common.DirectiveRunCommand{Command: loginExec})
+		} else {
+			dir = append(dir, common.DirectiveRunCommand{Command: "interactive"})
+		}
+
+		if loginRunRoot != "" {
+			dir = append(dir, common.DirectiveBuiltin{Name: "init", GuestFilename: "init"})
+
+			def := builder.NewBuildFsDefinition(dir, "fragments")
+
+			ctx := db.NewBuildContext(def)
+
+			f, err := db.Build(ctx, def, common.BuildOptions{})
+			if err != nil {
+				slog.Error("fatal", "err", err)
+				os.Exit(1)
+			}
+
+			frags, err := builder.ParseFragmentsBuilderResult(f)
+			if err != nil {
+				slog.Error("fatal", "err", err)
+				os.Exit(1)
+			}
+
+			if err := frags.ExtractAndRunScripts(loginRunRoot); err != nil {
+				slog.Error("fatal", "err", err)
+				os.Exit(1)
+			}
+
+			return nil
+		}
+
+		def := builder.NewBuildVmDefinition(
+			dir,
+			nil, nil,
+			loginOutput,
+			loginCpuCores, loginMemorySize, loginStorageSize,
+			"ssh", loginDebug,
+		)
+
+		if loginOutput != "" {
+			ctx := db.NewBuildContext(def)
+
+			f, err := db.Build(ctx, def, common.BuildOptions{})
+			if err != nil {
+				slog.Error("fatal", "err", err)
+				os.Exit(1)
+			}
+
+			fh, err := f.Open()
+			if err != nil {
+				return err
+			}
+			defer fh.Close()
+
+			out, err := os.Create(path.Base(loginOutput))
+			if err != nil {
+				return err
+			}
+			defer out.Close()
+
+			if _, err := io.Copy(out, fh); err != nil {
+				return err
+			}
+
+			return nil
+		} else {
+			ctx := db.NewBuildContext(def)
+			if _, err := db.Build(ctx, def, common.BuildOptions{
 				AlwaysRebuild: true,
 			}); err != nil {
 				slog.Error("fatal", "err", err)
 				os.Exit(1)
 			}
 
+			if common.IsVerbose() {
+				ctx.DisplayTree()
+			}
+
 			return nil
-		} else {
-			var pkgs []common.PackageQuery
-
-			for _, arg := range args {
-				q, err := common.ParsePackageQuery(arg)
-				if err != nil {
-					return err
-				}
-
-				pkgs = append(pkgs, q)
-			}
-
-			var dir []common.Directive
-
-			if loginBuilder == "" {
-				return fmt.Errorf("please specify a builder")
-			}
-
-			var tags common.TagList
-
-			tags = append(tags, "level3", "defaults")
-
-			if loginNoScripts || loginWriteRoot != "" {
-				tags = append(tags, "noScripts")
-			}
-
-			planDirective, err := builder.NewPlanDefinition(loginBuilder, pkgs, tags)
-			if err != nil {
-				return err
-			}
-
-			dir = append(dir, planDirective)
-
-			for _, filename := range loginFiles {
-				if strings.HasPrefix(filename, "http://") || strings.HasPrefix(filename, "https://") {
-					parsed, err := url.Parse(filename)
-					if err != nil {
-						return err
-					}
-
-					base := path.Base(parsed.Path)
-
-					dir = append(dir, common.DirectiveAddFile{
-						Definition: builder.NewFetchHttpBuildDefinition(filename, 0),
-						Filename:   path.Join("/root", base),
-					})
-				} else {
-					absPath, err := filepath.Abs(filename)
-					if err != nil {
-						return err
-					}
-
-					dir = append(dir, common.DirectiveLocalFile{
-						HostFilename: absPath,
-						Filename:     path.Join("/root", filepath.Base(absPath)),
-					})
-				}
-			}
-
-			for _, filename := range loginArchives {
-				var def common.BuildDefinition
-				if strings.HasPrefix(filename, "http://") || strings.HasPrefix(filename, "https://") {
-					def = builder.NewFetchHttpBuildDefinition(filename, 0)
-
-					parsed, err := url.Parse(filename)
-					if err != nil {
-						return err
-					}
-
-					filename = parsed.Path
-				} else {
-					hash, err := sha256HashFromFile(filename)
-					if err != nil {
-						return err
-					}
-
-					def = builder.NewConstantHashDefinition(hash, func() (io.ReadCloser, error) {
-						return os.Open(filename)
-					})
-				}
-
-				ark, err := detectArchiveExtractor(def, filename)
-				if err != nil {
-					return err
-				}
-
-				dir = append(dir, common.DirectiveArchive{Definition: ark, Target: "/root"})
-			}
-
-			if loginWriteRoot != "" {
-				dir = append(dir, common.DirectiveBuiltin{Name: "init", GuestFilename: "init"})
-
-				def := builder.NewBuildFsDefinition(dir, "tar")
-
-				ctx := db.NewBuildContext(def)
-
-				f, err := db.Build(ctx, def, common.BuildOptions{})
-				if err != nil {
-					slog.Error("fatal", "err", err)
-					os.Exit(1)
-				}
-
-				fh, err := f.Open()
-				if err != nil {
-					return err
-				}
-				defer fh.Close()
-
-				out, err := os.Create(path.Base(loginWriteRoot))
-				if err != nil {
-					return err
-				}
-				defer out.Close()
-
-				if _, err := io.Copy(out, fh); err != nil {
-					return err
-				}
-
-				return nil
-			}
-
-			if loginExec != "" {
-				dir = append(dir, common.DirectiveRunCommand{Command: loginExec})
-			} else {
-				dir = append(dir, common.DirectiveRunCommand{Command: "interactive"})
-			}
-
-			if loginRunRoot != "" {
-				dir = append(dir, common.DirectiveBuiltin{Name: "init", GuestFilename: "init"})
-
-				def := builder.NewBuildFsDefinition(dir, "fragments")
-
-				ctx := db.NewBuildContext(def)
-
-				f, err := db.Build(ctx, def, common.BuildOptions{})
-				if err != nil {
-					slog.Error("fatal", "err", err)
-					os.Exit(1)
-				}
-
-				frags, err := builder.ParseFragmentsBuilderResult(f)
-				if err != nil {
-					slog.Error("fatal", "err", err)
-					os.Exit(1)
-				}
-
-				if err := frags.ExtractAndRunScripts(loginRunRoot); err != nil {
-					slog.Error("fatal", "err", err)
-					os.Exit(1)
-				}
-
-				return nil
-			}
-
-			def := builder.NewBuildVmDefinition(
-				dir,
-				nil, nil,
-				loginOutput,
-				loginCpuCores, loginMemorySize, loginStorageSize,
-				"ssh", loginDebug,
-			)
-
-			if loginSaveConfig != "" {
-				w, err := os.Create(loginSaveConfig)
-				if err != nil {
-					return err
-				}
-				defer w.Close()
-
-				if err := builder.MarshalDefinition(w, def); err != nil {
-					return err
-				}
-
-				return nil
-			} else if loginOutput != "" {
-				ctx := db.NewBuildContext(def)
-
-				f, err := db.Build(ctx, def, common.BuildOptions{})
-				if err != nil {
-					slog.Error("fatal", "err", err)
-					os.Exit(1)
-				}
-
-				fh, err := f.Open()
-				if err != nil {
-					return err
-				}
-				defer fh.Close()
-
-				out, err := os.Create(path.Base(loginOutput))
-				if err != nil {
-					return err
-				}
-				defer out.Close()
-
-				if _, err := io.Copy(out, fh); err != nil {
-					return err
-				}
-
-				return nil
-			} else {
-				ctx := db.NewBuildContext(def)
-				if _, err := db.Build(ctx, def, common.BuildOptions{
-					AlwaysRebuild: true,
-				}); err != nil {
-					slog.Error("fatal", "err", err)
-					os.Exit(1)
-				}
-
-				if common.IsVerbose() {
-					ctx.DisplayTree()
-				}
-
-				return nil
-			}
 		}
 	},
 }
@@ -373,8 +336,6 @@ func init() {
 	loginCmd.PersistentFlags().IntVar(&loginStorageSize, "storage", 1024, "The amount of storage to allocate in the virtual machine in megabytes.")
 	loginCmd.PersistentFlags().BoolVar(&loginDebug, "debug", false, "Redirect output from the hypervisor to the host. the guest will exit as soon as the VM finishes startup.")
 	loginCmd.PersistentFlags().StringVarP(&loginExec, "exec", "E", "", "Run a different command rather than dropping into a shell.")
-	loginCmd.PersistentFlags().StringVar(&loginSaveConfig, "save-definition", "", "Serialize the definition to the specified filename.")
-	loginCmd.PersistentFlags().StringVarP(&loginLoadConfig, "load-definition", "c", "", "Run a virtual machine from a serialized definition.")
 	loginCmd.PersistentFlags().BoolVar(&loginNoScripts, "no-scripts", false, "Disable script execution.")
 	loginCmd.PersistentFlags().StringArrayVarP(&loginFiles, "file", "f", []string{}, "Specify local files/URLs to be copied into the virtual machine. URLs will be downloaded to the build directory first.")
 	loginCmd.PersistentFlags().StringArrayVarP(&loginArchives, "archive", "a", []string{}, "Specify archives to be copied into the virtual machine. A copy will be made in the build directory.")
