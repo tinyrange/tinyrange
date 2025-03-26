ad78ffeaa8131b1d6ccd13e47760807d74ddd297
Joshua Scarsbrook <jscarsbrook@gmail.com>
2025-03-26 23:00:59 +1000

    vmm: fixed acceleration on Windows Server 2019, closes #101

--- pkg/vmm/accelerate/accelerate_windows.go
+++ pkg/vmm/accelerate/accelerate_windows.go
@@ -6,28 +6,73 @@ import (
 	"syscall"
 	"unsafe"
 
+	"github.com/tinyrange/tinyrange/pkg/log"
 	"golang.org/x/sys/windows"
 )
+
+const WHvCapabilityCodeHypervisorPresent = 0x00000000
 
 func SupportsAcceleration() bool {
 	lib, err := windows.LoadLibrary("WinHVPlatform.dll")
 	if err != nil {
+		log.Warn("Failed to load WinHVPlatform.dll", "err", err)
 		return false
 	}
 	defer windows.FreeLibrary(lib)
 
 	addr, err := windows.GetProcAddress(lib, "WHvGetCapability")
 	if err != nil {
+		log.Warn("Failed to get WHvGetCapability address", "err", err)
 		return false
 	}
 
 	var buf uint64
 	var val uint32
 
-	_, _, winErr := syscall.SyscallN(addr, 0x00, uintptr(unsafe.Pointer(&buf)), unsafe.Sizeof(buf), uintptr(unsafe.Pointer(&val)))
+	_, _, winErr := syscall.SyscallN(
+		addr,
+		WHvCapabilityCodeHypervisorPresent,
+		uintptr(unsafe.Pointer(&buf)),
+		unsafe.Sizeof(buf),
+		uintptr(unsafe.Pointer(&val)),
+	)
+	if winErr == windows.ERROR_INVALID_PARAMETER {
+		bufSize := make([]byte, val)
+
+		_, _, winErr = syscall.SyscallN(
+			addr,
+			WHvCapabilityCodeHypervisorPresent,
+			uintptr(unsafe.Pointer(&bufSize)),
+			uintptr(val),
+			uintptr(unsafe.Pointer(&val)),
+		)
+		if winErr != windows.ERROR_SUCCESS {
+			log.Warn(
+				"Failed to call WHvGetCapability",
+				"err", winErr,
+				"errno", int64(syscall.Errno(winErr)),
+				"buf", buf,
+				"val", val,
+			)
+			return false
+		}
+	}
+
 	if winErr != windows.ERROR_SUCCESS {
+		log.Warn(
+			"Failed to call WHvGetCapability",
+			"err", winErr,
+			"errno", int64(syscall.Errno(winErr)),
+			"buf", buf,
+			"val", val,
+		)
 		return false
 	}
 
-	return buf != 0
+	if buf == 0 {
+		log.Warn("WHvGetCapability returned 0")
+		return false
+	}
+
+	return true
 }
