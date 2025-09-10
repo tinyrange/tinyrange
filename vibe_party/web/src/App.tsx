242c637fba2731cc2e65c825f4ed5b680c47654b
Joshua Scarsbrook <jscarsbrook@gmail.com>
2025-09-10 18:15:10 +1000

    vibe_party: added basic structure for tinyrange-web

---
+++ vibe_party/web/src/App.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+      Make pretty stuff here.
+    </div>
+  );
+}
+
+export default App;
